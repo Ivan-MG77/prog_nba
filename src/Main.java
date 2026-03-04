86816d55f76983126c3a25b7e9479783422937bc
ivanm <ivan.m.g.1791@gmail.com>
2026-03-04 18:52:49 +0100

    feat(mein): meter datos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        FranquiciaNBA franquiciaNBA = new FranquiciaNBA(15);
+
+        Jugador jugador1 = new Jugador(1,"Lebron", "Escolta", 12.4);
+        Jugador jugador2 = new Jugador(2,"Anteto", "Pivot", 15.2);
+        Jugador jugador3 = new Jugador(3,"Luka", "Base", 33.3);
+
+        System.out.println(franquiciaNBA.buscarJugador(1));
+
+        franquiciaNBA.ficharJugador(jugador1);
+        franquiciaNBA.ficharJugador(jugador2);
+        franquiciaNBA.ficharJugador(jugador3);
+
+        System.out.println(franquiciaNBA.mostrarPlantilla());
+
+        System.out.println("Total de jugadores en la platilla: " + franquiciaNBA.contarJugadoresActuales());
+    }
 }
