0bed4103d17511562fc76e29fc58ac67714d28dc
ivanm <ivan.m.g.1791@gmail.com>
2026-03-04 10:25:34 +0100

    feat(Jugador): Metodo Constructor de la clase Jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -5,4 +5,14 @@ public class Jugador {
     private String posicion;
     private double puntosPromedio;
 
+    public Jugador(int id, String nombre, String posicion, double puntosPromedio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.puntosPromedio = puntosPromedio;
+    }
+
+
+
+
 }
